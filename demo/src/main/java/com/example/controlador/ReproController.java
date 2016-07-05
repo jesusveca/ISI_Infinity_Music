d541a8cec298f69d0ec7b9f6a293e2f40d53be65
pikachu49 <julio31529@gmail.com>
2016-07-04 23:48:01 -0500

    main & register

--- demo/src/main/java/com/example/controlador/ReproController.java
+++ demo/src/main/java/com/example/controlador/ReproController.java
@@ -1,4 +1,4 @@
-package com.example.controlador;
+package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -6,12 +6,18 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import domain.Person;
 
 @Controller
-public class ReproductorControlador {
-	
+public class ReproController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	String home(ModelMap model) {
 		return "main";
 	}
+	
+
+	
 }
+
