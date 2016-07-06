cc5443e193c6ca60209974f64ce70c78070af495
pikachu49 <julio31529@gmail.com>
2016-07-06 00:36:41 -0500

    new project

---
+++ FinalPractico/reproductor/src/main/java/service/LoginService.java
@@ -0,0 +1,12 @@
+package service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+	
+	public boolean login(Integer usrId, String passwd){
+		return true;
+	}
+
+}
